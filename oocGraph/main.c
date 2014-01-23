b32cfd0c474656c9be7173d0277f702c09d28512
吴 歆韵 <Xavier@wumatoMacBook-Pro.local>
2014-01-23 17:35:56 +0800

    Initial Commit

---
+++ oocGraph/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  oocGraph
+//
+//  Created by 吴 歆韵 on 14-1-23.
+//  Copyright (c) 2014年 Xavier Woo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
